a06cbb738deb8ac2f2848beacb46b019c3d70384
vasilisdmr <vasilisdmr@gmail.com>
2023-04-13 18:18:15 +0200

    Add customer routes and some http requests for testing

---
+++ src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -0,0 +1,51 @@
+package com.example.routes
+
+import com.example.models.Customer
+import com.example.models.customers
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+            if (customers.isNotEmpty()) {
+                call.respond(customers)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.NotFound)
+            }
+        }
+        get("{id?}") {
+            val id =
+                call.parameters["id"] ?: return@get call.respondText("Missing id", status = HttpStatusCode.BadRequest)
+
+            val customer = customers.find { it.id == id } ?: return@get call.respondText(
+                "Customer with id: $id was not found!",
+                status = HttpStatusCode.NotFound
+            )
+
+            call.respond(customer)
+        }
+        post {
+            val customer = call.receive<Customer>()
+            customers.add(customer)
+
+            call.respondText("Customer created successfully", status = HttpStatusCode.Created)
+        }
+        delete("{id?}") {
+            val id = call.parameters["id"] ?: return@delete call.respondText(
+                "Missing id",
+                status = HttpStatusCode.BadRequest
+            )
+
+            if (customers.removeIf { it.id == id }) {
+                call.respondText("Customer deleted successfully", status = HttpStatusCode.NoContent)
+            } else {
+                call.respondText("Customer with id: $id, was not found!", status = HttpStatusCode.NotFound)
+            }
+
+        }
+    }
+}
