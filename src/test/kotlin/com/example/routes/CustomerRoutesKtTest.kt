31aba9b7b7212f111815c87ff09ed3e923763a5a
vasilisdmr <vasilisdmr@gmail.com>
2023-04-30 21:20:50 +0200

    Customer test refactor
    Since I am using the email field as a key to
    insert data within the repeatable migration, I
    modified test customer's data and I am deleting
    the newly created data after the test executed.

--- src/test/kotlin/com/example/routes/CustomerRoutesKtTest.kt
+++ src/test/kotlin/com/example/routes/CustomerRoutesKtTest.kt
@@ -1,39 +1,49 @@
 package com.example.routes
 
+import com.example.dao.Database
 import com.example.models.CustomerCreation
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
+import org.jooq.generated.Tables.CUSTOMERS
+import org.junit.After
 import kotlin.test.*
 
 
 class CustomerRoutesKtTest {
 
-    private val customer = CustomerCreation(
-        firstName = "John",
+    private val create = Database.getConnection()
+
+    private val testCustomer = CustomerCreation(
+        firstName = "Robert",
         lastName = "Doe",
-        email = "johndoe@domain.com"
+        email = "robertdoe@domain.com"
     )
 
-//    @Test
-//    fun `Customer creation should succeed`() =
-//        testApplication {
-//            // given
-//            val client = createClient {
-//                install(ContentNegotiation) {
-//                    json()
-//                }
-//            }
-//
-//            // when
-//            val customerResponse = client.post("/customer") {
-//                contentType(ContentType.Application.Json)
-//                setBody(customer)
-//            }
-//
-//            // then
-//            assertEquals(HttpStatusCode.Created, customerResponse.status)
-//        }
+    @After
+    fun cleanUp() {
+        create.deleteFrom(CUSTOMERS).where(CUSTOMERS.EMAIL.eq(testCustomer.email)).execute()
+    }
+
+    @Test
+    fun `Customer creation should succeed`() =
+        testApplication {
+            // given
+            val client = createClient {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }
+
+            // when
+            val customerResponse = client.post("/customer") {
+                contentType(ContentType.Application.Json)
+                setBody(testCustomer)
+            }
+
+            // then
+            assertEquals(HttpStatusCode.Created, customerResponse.status)
+        }
 }
