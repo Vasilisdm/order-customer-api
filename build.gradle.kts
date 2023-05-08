d956da9a9cc47b60b8cb0fd0ecd953ce311d9d02
vasilisdmr <vasilisdmr@gmail.com>
2023-05-08 20:13:57 +0200

    Remove unnecessary duplicated dependency and made order test execute correctly.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
--- src/test/kotlin/com/example/routes/OrderRouteTests.kt
+++ src/test/kotlin/com/example/routes/OrderRouteTests.kt
@@ -1,5 +1,7 @@
 package com.example.routes
 
+import com.example.models.OrderCreation
+import com.example.models.OrderItemCreation
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
@@ -9,32 +11,31 @@ import kotlin.test.*
 
 class OrderRouteTests {
 
-//
-//    private val orderItems = listOf(
-//        OrderItemCreation(item = 1, amount = 1, price = 159.99),
-//        OrderItemCreation(item = 2, amount = 1, price = 79.99),
-//        OrderItemCreation(item = 3, amount = 1, price = 129.99),
-//        OrderItemCreation(item = 4, amount = 1, price = 119.00),
-//    )
-//
-//    private val order = OrderCreation(id = 1, content = orderItems)
-//
-//    @Test
-//    fun `Post order should execute successfully`() = testApplication {
-//        // given
-//        val client = createClient {
-//            install(ContentNegotiation) {
-//                json()
-//            }
-//        }
-//
-//        // when
-//        val response = client.post("/order") {
-//            contentType(ContentType.Application.Json)
-//            setBody(order)
-//        }
-//
-//        // then
-//        assertEquals(HttpStatusCode.Created, response.status)
-//    }
+    private val orderItems = listOf(
+        OrderItemCreation(name = "OrderItem1", quantity = 1, price = 50.00),
+        OrderItemCreation(name = "OrderItem2", quantity = 1, price = 100.00),
+        OrderItemCreation(name = "OrderItem3", quantity = 1, price = 150.00),
+        OrderItemCreation(name = "OrderItem4", quantity = 1, price = 200.00),
+    )
+
+    private val order = OrderCreation(items = orderItems)
+
+    @Test
+    fun `Post order should execute successfully`() = testApplication {
+        // given
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        // when
+        val response = client.post("/order") {
+            contentType(ContentType.Application.Json)
+            setBody(order)
+        }
+
+        // then
+        assertEquals(HttpStatusCode.Created, response.status)
+    }
 }
